55c2c4588071c782d79fd3789d623aa9fc2fec11
Denge12345 <3197451416@qq.com>
2026-04-28 18:45:57 +0800

    modify code

--- CF1183/E.cpp
+++ CF1183/E.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 
 void solve(){
-    
+    int n, k; cin >> n >> k;
+    string s; cin >> s;
+    set<string> st;
+    queue<string> q;
+    q.push(s);
+    st.insert(s);
+    int ans = 0;
+    while(!q.empty() && st.size() < k){
+        string cur = q.front();
+        q.pop();
+        for(int i = 0; i < cur.size(); i++){
+            string ns = cur;
+            ns.erase(i, 1);
+            if(st.count(ns) || st.size() + 1 > k) continue;
+            st.insert(ns);
+            q.push(ns);
+            ans += (n - ns.size());
+        }
+    }
+    cout << (st.size() < k ? -1 : ans) << '\n';
 }
 
 int main(){
