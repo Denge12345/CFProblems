927cffaaf17f80b0034197224d6f1b78937f2954
Denge12345 <3197451416@qq.com>
2026-04-30 08:19:02 +0800

    modify code

---
+++ CF1144/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve(){
+    string s; cin >> s;
+    vector<int> arr;
+    for(char c : s){
+        arr.push_back(c - 'a');
+    }
+    sort(arr.begin(), arr.end());
+    for(int i = 1; i < arr.size(); i++){
+        if(arr[i] != arr[i - 1] + 1){
+            cout << "No" << '\n';
+            return;
+        }
+    }
+    cout << "Yes" << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
