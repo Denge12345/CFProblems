6084d25c78df4b994d42f92fdac56ec2d510fccd
Denge12345 <3197451416@qq.com>
2026-05-02 15:20:21 +0800

    modify code

--- CF1374/D.cpp
+++ CF1374/D.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 void solve(){
-    
+    int n, k; cin >> n >> k;
+    map<ll, ll> need;
+    for(int i = 1; i <= n; i++){
+        int a; cin >> a;
+        if(a % k != 0) need[k - (a % k)]++;
+    }
+    if(need.empty()){
+        cout << 0 << '\n';
+        return;
+    }
+    ll ans = 0;
+    for(const auto& [r, c] : need){
+        ans = max(ans, r + (c - 1) * k + 1);
+    }
+    cout << ans << '\n';
 }
 
 int main(){
