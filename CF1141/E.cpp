aea4a8e0ecec4104bb14a857cfb8a532342189dc
Denge12345 <3197451416@qq.com>
2026-05-04 13:08:55 +0800

    modify code

--- CF1141/E.cpp
+++ CF1141/E.cpp
@@ -1,8 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+const int N = 2e5 + 9;
+ll a[N];
 
 void solve(){
-    
+    ll H; cin >> H;
+    int n; cin >> n;
+    ll sum = 0, mx = 0, h = H;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        sum -= a[i];
+        h += a[i];
+        if(h <= 0){
+            cout << i + 1 << '\n';
+            return;
+        }
+        mx = max(mx, sum);
+    }
+    if(sum <= 0){
+        cout << -1 << '\n';
+        return;
+    }
+    ll t = (H - mx) / sum;
+    H -= t * sum;
+    ll ans = t * n;
+    for(int i = 0; ; i++){
+        H += a[i % n];
+        ans++;
+        if(H <= 0){
+            cout << ans << '\n';
+            return;
+        }
+    }
 }
 
 int main(){
