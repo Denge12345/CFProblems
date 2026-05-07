ed58202f84da790f460f3d5b18fe11bdcc3f02f3
Denge12345 <3197451416@qq.com>
2026-05-07 15:40:49 +0800

    modify code

---
+++ CF1157/C1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2e5 + 9;
+int a[N];
+
+void solve(){
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    int l = 1, r = n;
+    string ans;
+    stack<int> stk;
+    stk.push(0);
+    while(l <= r){
+        int lst = stk.top();
+        bool lok = (a[l] > lst);
+        bool rok = (a[r] > lst);
+        if(!lok && !rok) break;
+        if(lok && rok){
+            if(a[l] < a[r]){
+                ans += 'L';
+                stk.push(a[l++]);
+            }else{
+                ans += 'R';
+                stk.push(a[r--]);
+            }
+        }else if(lok){
+            ans += 'L';
+            stk.push(a[l++]);
+        }else{
+            ans += 'R';
+            stk.push(a[r--]);
+        }
+    }
+    cout << ans.size() << '\n';
+    cout << ans << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
