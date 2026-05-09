2ae89f951c7aeebe7c67357d762d6d9080392d14
Denge12345 <3197451416@qq.com>
2026-05-09 16:49:39 +0800

    modify code

--- CF141/B.cpp
+++ CF141/B.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 void solve(){
-    
+    int a, x, y; cin >> a >> x >> y;
+    if(y % a == 0 || y < 0 || abs(x) >= a){
+        cout << -1 << '\n';
+        return;
+    }
+    int k = y / a;
+    if(k == 0 || k % 2 == 1){
+        if(abs(2 * x) < a) cout << max(1, (k + 1) / 2 * 3 - 1) << '\n';
+        else cout << -1 << '\n';
+    }else if(k % 2 == 0){
+        if(x != 0) cout << k / 2 * 3 + (x + a) / a << '\n';
+        else cout << -1 << '\n';
+    }
 }
 
 int main(){
