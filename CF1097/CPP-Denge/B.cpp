56791ef3e9feacedddbec110511c69b6e13bfaef
Denge12345 <3197451416@qq.com>
2026-05-15 21:28:21 +0800

    modify code

---
+++ CF1097/CPP-Denge/B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 20;
+int a[N];
+
+void solve(){
+	int n; cin >> n;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    bool ok = 0;
+    for(int status = 0; status < (1 << n); status++){
+        
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while(t--){
+    	solve();	
+	}
+    return 0;
+}
