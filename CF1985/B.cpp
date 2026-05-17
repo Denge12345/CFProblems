8390f6f8fd6352cb17fcdad602174e298197e5c0
Denge12345 <3197451416@qq.com>
2026-05-17 23:49:19 +0800

    modify code

---
+++ CF1985/B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    cout << (n == 3 ? 3 : 2) << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
