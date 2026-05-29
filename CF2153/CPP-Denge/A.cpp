6ef7d1e3e53fa5a205652f0d8143bb05fba5eeb4
Denge12345 <3197451416@qq.com>
2026-05-29 21:22:51 +0800

    modify code

---
+++ CF2153/CPP-Denge/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    set<int> st;
+    for(int i = 1; i <= n; i++){
+        int b; cin >> b;
+        st.insert(b);
+    }
+    cout << st.size() << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
