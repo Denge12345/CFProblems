6baa81de6112287016131452a1310631a889a379
Denge12345 <3197451416@qq.com>
2026-06-16 19:52:19 +0800

    modify code

--- CF1165/CPP-Denge/D.cpp
+++ CF1165/CPP-Denge/D.cpp
@@ -3,7 +3,24 @@ using namespace std;
 using ll = long long;
 
 void solve(){
-	
+	int n; cin >> n;
+    vector<ll> a, d;
+    for(int i = 1; i <= n; i++){
+        ll x; cin >> x;
+        a.push_back(x);
+    }
+    sort(a.begin(), a.end());
+    ll ans = a[0] * a[n - 1];
+    for(ll i = 2; i * i <= ans; i++){
+        if(ans % i == 0){
+            d.push_back(i);
+            if(i != ans / i){
+                d.push_back(ans / i);
+            }
+        }
+    }
+    sort(d.begin(), d.end());
+    cout << (a == d ? ans : -1) << '\n';
 }
 
 int main() {
