c1f025624b52233118e5e5e4292835ec43b3d111
Denge12345 <3197451416@qq.com>
2026-06-27 20:56:58 +0800

    modify code

--- CF628/CPP-Denge/C.cpp
+++ CF628/CPP-Denge/C.cpp
@@ -3,7 +3,31 @@ using namespace std;
 using ll = long long;
 
 void solve(){
-    
+    int n, k; cin >> n >> k;
+    string s; cin >> s;
+    int maxDist = 0;
+    for(char c : s){
+        maxDist += max(c - 'a', 'z' - c);
+    }
+    if(maxDist < k){
+        cout << -1 << '\n';
+        return;
+    }
+    string ans = s;
+    for(int i = 0; i < n && k; i++){
+        int toA = ans[i] - 'a';
+        int toZ = 'z' - ans[i];
+        if(toA > toZ){
+            int d = min(k, toA);
+            ans[i] -= d;
+            k -= d;
+        }else{
+            int d = min(k, toZ);
+            ans[i] += d;
+            k -= d;
+        }
+    }
+    cout << ans << '\n';
 }
 
 int main() {
