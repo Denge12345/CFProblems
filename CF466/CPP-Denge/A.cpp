8f1c97bf9e232c399dc958687eee9064fe444c3b
Denge12345 <3197451416@qq.com>
2026-07-17 18:48:26 +0800

    modify code

---
+++ CF466/CPP-Denge/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+    int ans = INT_MAX;
+    for(int i = 0; i <= n; i++){
+        int left = (n - i + m - 1) / m;
+        ans = min(ans, i * a + left * b);
+    }
+    cout << ans << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
